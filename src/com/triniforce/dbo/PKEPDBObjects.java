b6ccc2e7f670ac6cd267df7bf0642c41e04910ae
maxim.ge <maxim.ge@0957a958-f1c7-11dd-ac65-f74f249cfaac>
2012-12-18 09:35:19 +0000

    filetypes

--- src/com/triniforce/dbo/PKEPDBObjects.java
+++ src/com/triniforce/dbo/PKEPDBObjects.java
@@ -8,6 +8,7 @@ package com.triniforce.dbo;
 import com.triniforce.extensions.EExtensionPointNotFound;
 import com.triniforce.extensions.IPKExtension;
 import com.triniforce.extensions.PKExtensionPoint;
+import com.triniforce.utils.ApiStack;
 
 public class PKEPDBObjects extends PKExtensionPoint{
 	
@@ -18,13 +19,17 @@ public class PKEPDBObjects extends PKExtensionPoint{
 	@Override
 	public IPKExtension putExtension(String extensionId, Object obj)
 			throws EExtensionPointNotFound {
-		
-		IPKExtension res = super.putExtension(extensionId, obj);
-		IDBObject dbo = res.getInstance();
-		for(IDBObject synth : dbo.synthDBObjects()){
-			super.putExtension((String) synth.getKey(), synth);
+		ApiStack.pushInterface(PKEPDBObjects.class, this);
+		try{
+    		IPKExtension res = super.putExtension(extensionId, obj);
+    		IDBObject dbo = res.getInstance();
+    		for(IDBObject synth : dbo.synthDBObjects()){
+    			super.putExtension((String) synth.getKey(), synth);
+    		}
+    		return res;
+		}finally{
+		    ApiStack.popInterface(1);		    
 		}
-		return res;
 	}
 	
 }
