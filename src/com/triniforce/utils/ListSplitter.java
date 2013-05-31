bbd44b0dc41aab839fa443fdfff6c038319dfbe2
maxim.ge <maxim.ge@0957a958-f1c7-11dd-ac65-f74f249cfaac>
2013-05-31 12:34:26 +0000

    ListSplitter

---
+++ src/com/triniforce/utils/ListSplitter.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright(C) Triniforce
+ * All Rights Reserved.
+ *
+ */ 
+package com.triniforce.utils;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ListSplitter implements Iterable<List>{
+
+    private final Iterable m_src;
+    private final int m_size;
+
+    public ListSplitter(Iterable src, int size){
+        m_src = src;
+        m_size = size;
+
+    }
+    
+    class Iter implements Iterator{
+        private Iterator m_iterator;
+        List m_cur;
+        public Iter(){
+            m_iterator = m_src.iterator();
+            m_cur = new ArrayList<Object>(m_size);
+        }
+
+        public boolean hasNext(){
+            return m_iterator.hasNext();
+        }
+
+        public Object next() {
+            while(m_iterator.hasNext() && m_cur.size() < m_size){
+                m_cur.add(m_iterator.next());
+            }
+            List res = m_cur;
+            m_cur = new ArrayList<Object>(m_size);
+            return res;
+        }
+
+        public void remove() {
+        }
+        
+    }
+    
+    public Iterator<List> iterator() {
+        return new Iter();
+    }
+
+}
