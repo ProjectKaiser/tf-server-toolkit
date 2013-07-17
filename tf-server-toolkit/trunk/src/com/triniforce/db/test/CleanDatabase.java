beba4c36caba7b544d6b3ee968cd10bf3ab7c708
alexeyivakov <alexeyivakov@0957a958-f1c7-11dd-ac65-f74f249cfaac>
2013-07-17 09:30:31 +0000

    test fix

--- tf-server-toolkit/trunk/src/com/triniforce/db/test/CleanDatabase.java
+++ tf-server-toolkit/trunk/src/com/triniforce/db/test/CleanDatabase.java
@@ -35,127 +35,133 @@ public class CleanDatabase {
             DBTestCase.getDataSource().setMaxActive(0);
             DBTestCase.getDataSource().setMaxIdle(1);
             DBTestCase.getDataSource().setMaxActive(1);
-            run(test);
+            run(test,  ApiAlgs.getLog(test));
 
             test.trace("Ok. Done.");
         } finally {
-//            test.tearDown();
+            test.tearDown();
         }
 
     }
 
     public static void run(DBTestCase test) throws Exception {
-        Connection conn = test.getConnection();
-        try {
-        	run(test, ApiAlgs.getLog(test), conn);
-        } finally {
+    	test.setUp();
+    	try{
+//        Connection conn = test.getNewConnection();
+//        try {
+        	run(test, ApiAlgs.getLog(test));
+//        } finally {
 //            conn.close();
-        }
+//        }
+    	}finally{
+    		test.tearDown();
+    	}
     }
     
-    public static void run(DBTestCase test, Log log, Connection conn) throws Exception {
-    	
-//    	String schem = null;
-    	DbType dbType = DBTestCase.getDbType();
-    	boolean autoCommmit = conn.getAutoCommit();
-    	try{
-	        if (dbType.equals(DbType.MSSQL)){
-	            conn.setAutoCommit(true);
-//	            schem = "dbo";
-	        }
-	        else
-	            conn.setAutoCommit(false);
-	
-	        ActualStateBL as = new ActualStateBL(conn);
-	        Set<String> dbnames = new HashSet<String>(as.getDbTableNames());
-//	        dbnames.remove(ActualStateBL.ACT_STATE_TABLE);
-//	        dbnames.remove(as.getDBName(TIndexNames.class.getName()));
-	        conn = test.reopenConnection();
-	        conn.setAutoCommit(false);
-	        DatabaseMetaData md = conn.getMetaData();
-//	        conn.commit();
-//	        ResultSet rs = md.getTables(conn.getCatalog(), schem, "%",
-//	                new String[] { "TABLE", "VIEW" });
-	        for (String dbName  : dbnames) {
-//	            String dbName = rs.getString("TABLE_NAME");
-//	            if("VIEW".equals(rs.getString("TABLE_TYPE").toUpperCase())){
-//		            log.trace(String.format("Delete view %s.", dbName));
-//		        	conn.createStatement().execute("DROP VIEW "+dbName);            	
-//	            }
-//	            else{
-		            
-		            log.trace(String.format("Delete table %s.", dbName));
+    public static void run(DBTestCase test, Log log) throws Exception {
+
+    		Connection conn = test.reopenConnection();
+	//    	String schem = null;
+	    	DbType dbType = DBTestCase.getDbType();
+	    	boolean autoCommmit = conn.getAutoCommit();
+	    	try{
+		        if (dbType.equals(DbType.MSSQL)){
+		            conn.setAutoCommit(true);
+	//	            schem = "dbo";
+		        }
+		        else
+		            conn.setAutoCommit(false);
 		
-		            try {
-		                dropTable(conn, dbName);
-		            } catch (SQLException e) {
-		
-		                // Search for exported foreign keys on table being deleted
-		                ResultSet fkRs = md.getExportedKeys(null, null,dbName);
-		                String oldFkTab = null, oldFkName = null;
-		                while (fkRs.next()) {
-		                    String fkTab = fkRs.getString("FKTABLE_NAME");
-		                    String fkName = fkRs.getString("FK_NAME");
-		                    log.trace(String.format("Delete exported key %s.%s.",
-		                            fkTab, fkName));
-		                    if (!(fkTab.equals(oldFkTab) && fkName
-		                            .equals(oldFkName))) {
-		                        PreparedStatement ps = null;
-		                        if (DBTestCase.getDbType().equals(DbType.MYSQL))
-		                            ps = conn
-		                                    .prepareStatement("ALTER TABLE "
-		                                            + fkTab + " DROP FOREIGN KEY "
-		                                            + fkName);
-		                        else
-		                            ps = conn.prepareStatement("ALTER TABLE "
-		                                    + fkTab + " DROP CONSTRAINT " + fkName);
-		                        ps.execute();
-		                        ps.close();
-		                    }
-		                    oldFkTab = fkTab;
-		                    oldFkName = fkName;
-		                }
-		                try{
-		                	dropTable(conn, dbName);
-		                }catch(SQLNestedException e2){
-		                	log.trace(String.format("Drop table  failed %s.", dbName));
-//		                	log.trace("cause", e2);
-		                }
-		                catch(Exception e2){
-		                	log.trace("unresolved drop : " + dbName);
-//		                	PreparedStatement ps = conn.prepareStatement("select * from MON$ATTACHMENTS");
-//		                	ResultSet rs = ps.executeQuery();
-//		                	while(rs.next()){
-//		                		log.trace("conn: " + rs.getTimestamp("MON$TIMESTAMP"));
-//		                	}
-//		                	throw e2;
-		                	
-		                }
-//		                as.removeTable(as.getAppName(dbName));
-		            }
-//	            }
-	        }
-//	        as.flush(conn);
-	        conn.commit();
-//	        rs.close();
-	        // if database is firebird
-	        // drop all client generators
-	        if(DbType.FIREBIRD.equals(dbType)){
-	        	Statement st = conn.createStatement();
-	        	ResultSet rs = st.executeQuery("select RDB$GENERATOR_NAME from RDB$GENERATORS where RDB$SYSTEM_FLAG is null");
-	        	while(rs.next()){
-	        		Statement st2 = conn.createStatement();
-	        		st2.execute("DROP GENERATOR "+rs.getString(1));
-	        		st2.close();
-	        	}
-	        	rs.close();
-	        	st.close();
-	        }
-	        if (!conn.getAutoCommit())
-	            conn.commit();
-    	} finally{
-    		conn.setAutoCommit(autoCommmit);
-    	}
+		        ActualStateBL as = new ActualStateBL(conn);
+		        Set<String> dbnames = new HashSet<String>(as.getDbTableNames());
+	//	        dbnames.remove(ActualStateBL.ACT_STATE_TABLE);
+	//	        dbnames.remove(as.getDBName(TIndexNames.class.getName()));
+		        conn = test.reopenConnection();
+		        conn.setAutoCommit(false);
+		        DatabaseMetaData md = conn.getMetaData();
+	//	        conn.commit();
+	//	        ResultSet rs = md.getTables(conn.getCatalog(), schem, "%",
+	//	                new String[] { "TABLE", "VIEW" });
+		        for (String dbName  : dbnames) {
+	//	            String dbName = rs.getString("TABLE_NAME");
+	//	            if("VIEW".equals(rs.getString("TABLE_TYPE").toUpperCase())){
+	//		            log.trace(String.format("Delete view %s.", dbName));
+	//		        	conn.createStatement().execute("DROP VIEW "+dbName);            	
+	//	            }
+	//	            else{
+			            
+			            log.trace(String.format("Delete table %s.", dbName));
+			
+			            try {
+			                dropTable(conn, dbName);
+			            } catch (SQLException e) {
+			
+			                // Search for exported foreign keys on table being deleted
+			                ResultSet fkRs = md.getExportedKeys(null, null,dbName);
+			                String oldFkTab = null, oldFkName = null;
+			                while (fkRs.next()) {
+			                    String fkTab = fkRs.getString("FKTABLE_NAME");
+			                    String fkName = fkRs.getString("FK_NAME");
+			                    log.trace(String.format("Delete exported key %s.%s.",
+			                            fkTab, fkName));
+			                    if (!(fkTab.equals(oldFkTab) && fkName
+			                            .equals(oldFkName))) {
+			                        PreparedStatement ps = null;
+			                        if (DBTestCase.getDbType().equals(DbType.MYSQL))
+			                            ps = conn
+			                                    .prepareStatement("ALTER TABLE "
+			                                            + fkTab + " DROP FOREIGN KEY "
+			                                            + fkName);
+			                        else
+			                            ps = conn.prepareStatement("ALTER TABLE "
+			                                    + fkTab + " DROP CONSTRAINT " + fkName);
+			                        ps.execute();
+			                        ps.close();
+			                    }
+			                    oldFkTab = fkTab;
+			                    oldFkName = fkName;
+			                }
+			                try{
+			                	dropTable(conn, dbName);
+			                }catch(SQLNestedException e2){
+			                	log.trace(String.format("Drop table  failed %s.", dbName));
+	//		                	log.trace("cause", e2);
+			                }
+			                catch(Exception e2){
+			                	log.trace("unresolved drop : " + dbName);
+	//		                	PreparedStatement ps = conn.prepareStatement("select * from MON$ATTACHMENTS");
+	//		                	ResultSet rs = ps.executeQuery();
+	//		                	while(rs.next()){
+	//		                		log.trace("conn: " + rs.getTimestamp("MON$TIMESTAMP"));
+	//		                	}
+	//		                	throw e2;
+			                	
+			                }
+	//		                as.removeTable(as.getAppName(dbName));
+			            }
+	//	            }
+		        }
+	//	        as.flush(conn);
+		        conn.commit();
+	//	        rs.close();
+		        // if database is firebird
+		        // drop all client generators
+		        if(DbType.FIREBIRD.equals(dbType)){
+		        	Statement st = conn.createStatement();
+		        	ResultSet rs = st.executeQuery("select RDB$GENERATOR_NAME from RDB$GENERATORS where RDB$SYSTEM_FLAG is null");
+		        	while(rs.next()){
+		        		Statement st2 = conn.createStatement();
+		        		st2.execute("DROP GENERATOR "+rs.getString(1));
+		        		st2.close();
+		        	}
+		        	rs.close();
+		        	st.close();
+		        }
+		        if (!conn.getAutoCommit())
+		            conn.commit();
+	    	} finally{
+	    		conn.setAutoCommit(autoCommmit);
+	    	}
     }
 
     private static void dropTable(Connection conn, String dbName)
@@ -164,4 +170,6 @@ public class CleanDatabase {
         ps.execute();
         ps.close();
     }
+    
+    
 }
