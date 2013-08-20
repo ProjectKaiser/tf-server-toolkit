cfd7e4d22b5806604864d5479fb3020b4e766446
maxim.ge <maxim.ge@0957a958-f1c7-11dd-ac65-f74f249cfaac>
2013-08-20 10:29:20 +0000

    messagebus

--- tf-server-toolkit/trunk/src/com/triniforce/messagebus/BMMsg.java
+++ tf-server-toolkit/trunk/src/com/triniforce/messagebus/BMMsg.java
@@ -7,8 +7,15 @@ package com.triniforce.messagebus;
 
 public class BMMsg extends BM{
 
-	public BMMsg(Object data) {
+	private final String m_url;
+
+    public BMMsg(String url, Object data) {
 		super(data);
+        m_url = url;
 	}
+
+    public String getUrl() {
+        return m_url;
+    }
 
 }
