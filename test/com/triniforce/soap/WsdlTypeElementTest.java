1d78fb0e2ec69c7109c7303459e0b4f0f183620c
alexeyivakov <AlexeyIvakov@gmail.com>
2013-09-17 12:38:03 +0000

    Soap primotive types https://offsxp.office.sigma-soft.ru:8443/sigma?fileid=348438

--- test/com/triniforce/soap/WsdlTypeElementTest.java
+++ test/com/triniforce/soap/WsdlTypeElementTest.java
@@ -16,6 +16,7 @@ public class WsdlTypeElementTest extends TFTestCase {
 		private int prop;
 		private boolean prop2;
 		private Boolean prop3;
+		private Long prop4;
 
 		public void setProp(int prop) {
 			this.prop = prop;
@@ -40,6 +41,16 @@ public class WsdlTypeElementTest extends TFTestCase {
 		public Boolean getProp3() {
 			return prop3;
 		}
+
+		public Long getProp4() {
+			return prop4;
+		}
+
+		public void setProp4(Long prop4) {
+			this.prop4 = prop4;
+		}
+		
+		
 	} 
 	
 	@Override
@@ -54,7 +65,11 @@ public class WsdlTypeElementTest extends TFTestCase {
 		assertEquals("boolean", def.getProp("prop3").getType().getName());
 		
 		res = new WsdlDescription.WsdlTypeElement("n1", def.getProp("prop3").getType(), true, 1);;
-		assertEquals(1, res.getMinOccur());
+		assertEquals(0, res.getMinOccur());
+		assertTrue(res.isNillable());
+		
+		res = new WsdlDescription.WsdlTypeElement("n1", def.getProp("prop4").getType(), true, 1);;
+		assertEquals(0, res.getMinOccur());
 		assertTrue(res.isNillable());
 	}
 }
