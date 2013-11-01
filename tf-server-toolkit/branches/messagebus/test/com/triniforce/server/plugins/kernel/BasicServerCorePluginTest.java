d18c3f0aec97b3f51125de258586b2a32119c93e
maxim.ge <maxim.ge@0957a958-f1c7-11dd-ac65-f74f249cfaac>
2013-11-01 13:20:53 +0000

    merge with trunk

--- tf-server-toolkit/branches/messagebus/test/com/triniforce/server/plugins/kernel/BasicServerCorePluginTest.java
+++ tf-server-toolkit/branches/messagebus/test/com/triniforce/server/plugins/kernel/BasicServerCorePluginTest.java
@@ -9,6 +9,7 @@ import com.triniforce.db.test.BasicServerTestCase;
 import com.triniforce.server.plugins.kernel.ep.srv_ev.PKEPServerEvents;
 import com.triniforce.server.srvapi.ITaskExecutors;
 import com.triniforce.server.srvapi.IBasicServer.Mode;
+import com.triniforce.server.srvapi.ITimedLock2;
 import com.triniforce.utils.ApiStack;
 
 public class BasicServerCorePluginTest extends BasicServerTestCase {
@@ -19,6 +20,7 @@ public class BasicServerCorePluginTest extends BasicServerTestCase {
         getServer().enterMode(Mode.Running);
         try{
             ApiStack.getInterface(ITaskExecutors.class);
+            ApiStack.getInterface(ITimedLock2.class);
             getServer().getExtensionPoint(PKEPServerEvents.class);
         }finally{
             getServer().leaveMode();
