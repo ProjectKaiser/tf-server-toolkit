8ff6fb90f09150837fce7c75a795cc0a035ba9ff
maxim.ge <maxim.ge@0957a958-f1c7-11dd-ac65-f74f249cfaac>
2013-12-16 14:14:37 +0000

    ICommitable and periodical tasks

--- tf-server-toolkit/trunk/src/com/triniforce/server/plugins/kernel/ep/api/PKEPAPIPeriodicalTask.java
+++ tf-server-toolkit/trunk/src/com/triniforce/server/plugins/kernel/ep/api/PKEPAPIPeriodicalTask.java
@@ -6,7 +6,12 @@
 package com.triniforce.server.plugins.kernel.ep.api;
 
 import com.triniforce.server.plugins.kernel.PeriodicalTasksExecutor.BasicPeriodicalTask;
+import com.triniforce.server.srvapi.ISrvSmartTranFactory;
+import com.triniforce.utils.ICommitable;
 
-public abstract class PKEPAPIPeriodicalTask extends BasicPeriodicalTask implements IPKEPAPI{
+public abstract class PKEPAPIPeriodicalTask extends BasicPeriodicalTask implements IPKEPAPI, ICommitable{
+    public void commit(){
+        ISrvSmartTranFactory.Helper.commit();
+    }
 
 }
