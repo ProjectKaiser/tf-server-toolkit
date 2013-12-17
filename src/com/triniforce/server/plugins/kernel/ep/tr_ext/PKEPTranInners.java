6d0c4bf72e48a36b9e5debb645ae1ae98bde44a5
maxim.ge <maxim.ge@0957a958-f1c7-11dd-ac65-f74f249cfaac>
2013-12-17 13:19:41 +0000

    TODO

--- src/com/triniforce/server/plugins/kernel/ep/tr_ext/PKEPTranInners.java
+++ src/com/triniforce/server/plugins/kernel/ep/tr_ext/PKEPTranInners.java
@@ -10,6 +10,7 @@ import com.triniforce.server.srvapi.ISrvSmartTranFactory.ITranExtender;
 
 public class PKEPTranInners extends PKExtensionPoint{
     public PKEPTranInners() {
+        //TODO why single?
         setSingleExtensionInstances(true);
         setExtensionClass(ITranExtender.class);
     }
