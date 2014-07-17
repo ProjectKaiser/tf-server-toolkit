8861eb6e0ada44c10e6460ccafd98fba54a8e64e
maxim.ge <maxim.ge@0957a958-f1c7-11dd-ac65-f74f249cfaac>
2014-07-17 13:21:25 +0000

    Drivers loading https://offsxp.office.sigma-soft.ru:8443/sigma?fileid=368480

--- test/com/triniforce/server/plugins/kernel/ep/external_classes/PKEPExternalClassesTest.java
+++ test/com/triniforce/server/plugins/kernel/ep/external_classes/PKEPExternalClassesTest.java
@@ -10,7 +10,7 @@ import java.io.Serializable;
 import java.util.Collection;
 
 import com.triniforce.db.test.BasicServerRunningTestCase;
-import com.triniforce.extensions.IPKExtensionPoint;
+import com.triniforce.extensions.PKPlugin;
 import com.triniforce.extensions.PluginsLoaderTest;
 import com.triniforce.server.srvapi.IPlugin;
 import com.triniforce.utils.ApiStack;
@@ -28,46 +28,67 @@ public class PKEPExternalClassesTest extends BasicServerRunningTestCase {
         }
     }
     
+    public class MyPlugin extends PKPlugin{
+
+        @Override
+        public void doRegistration() {
+
+            ClassesFolder cfAll = new ClassesFolder_All(folder1);
+            ClassesFolder cfOnlyClasses = new ClassesFolder_OnlyClasses(folder2);
+            putExtension(PKEPExternalClasses.class, cfAll.getClass().getName(), cfAll);
+            putExtension(PKEPExternalClasses.class, cfOnlyClasses.getClass().getName(), cfOnlyClasses);
+            
+        }
+
+        @Override
+        public void doExtensionPointsRegistration() {
+        }
+        
+    }
+    
+    File folder1;
+    File folder2;
+    
+    @Override
+    protected void setUp() throws Exception {
+        addPlugin(new MyPlugin());
+        folder1 = new File(getTmpFolder(this), "f1");
+        folder1.mkdirs();
+        folder2 = new File(getTmpFolder(this), "f2");
+        folder2.mkdirs();
+        PluginsLoaderTest.copyTestPlugins(folder1);
+        PluginsLoaderTest.copyClasses(folder2);
+        
+        super.setUp();
+        
+
+    }
+    
     
     @Override
     public void test() throws Exception {
-        
-        File folder1 = new File(getTmpFolder(this), "f1");
-        folder1.mkdirs();
-        File folder2 = new File(getTmpFolder(this), "f2");
-        folder2.mkdirs();
-        
-        PluginsLoaderTest.copyTestPlugins(folder1);
-        PluginsLoaderTest.copyClasses(folder2);
-
-        ClassesFolder cfAll = new ClassesFolder_All(folder1);
-        ClassesFolder cfOnlyClasses = new ClassesFolder_OnlyClasses(folder2);
-        
-        IPKExtensionPoint ep = m_server.getExtensionPoint(PKEPExternalClasses.class);
-        ep.putExtension(cfAll);
-        ep.putExtension(cfOnlyClasses);
         
         IExternalClasses ec = ApiStack.getInterface(IExternalClasses.class);
         //test all jars
         {
             Class fcls = ClassesFolder_All.class;
-            Collection<Class> cls = ec.listClassesOfType(fcls, Object.class);
-            assertEquals(4, cls.size());
-            cls = ec.listClassesOfType(fcls, IPlugin.class);
-            assertEquals(2, cls.size());
-            cls = ec.listClassesOfType(fcls, Serializable.class);
-            assertEquals(1, cls.size());
-            cls = ec.listClassesOfType(fcls, Cloneable.class);
-            assertEquals(1, cls.size());
+            Collection<Class> clss = ec.listClassesOfType(fcls, Object.class);
+            assertEquals(4, clss.size());
+            clss = ec.listClassesOfType(fcls, IPlugin.class);
+            assertEquals(2, clss.size());
+            clss = ec.listClassesOfType(fcls, Serializable.class);
+            assertEquals(1, clss.size());
+            clss = ec.listClassesOfType(fcls, Cloneable.class);
+            assertEquals(1, clss.size());
         }
         {
             Class fcls = ClassesFolder_OnlyClasses.class;
-            Collection<Class> cls = ec.listClassesOfType(fcls, IPlugin.class);
-            assertEquals(0, cls.size());
-            cls = ec.listClassesOfType(fcls, Serializable.class);
-            assertEquals(1, cls.size());
-            cls = ec.listClassesOfType(fcls, Cloneable.class);
-            assertEquals(1, cls.size());        
+            Collection<Class> clss = ec.listClassesOfType(fcls, IPlugin.class);
+            assertEquals(0, clss.size());
+            clss = ec.listClassesOfType(fcls, Serializable.class);
+            assertEquals(1, clss.size());
+            clss = ec.listClassesOfType(fcls, Cloneable.class);
+            assertEquals(1, clss.size());        
         }
 
         
