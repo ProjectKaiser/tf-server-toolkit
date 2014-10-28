a275a22de71fb87503c09709e162e73bdc5cf3f4
maxim.ge <maxim.ge@0957a958-f1c7-11dd-ac65-f74f249cfaac>
2014-10-28 12:29:21 +0000

    Cleanup logging

--- test/com/triniforce/server/srvapi/InitFinitTaskWrapperTest.java
+++ test/com/triniforce/server/srvapi/InitFinitTaskWrapperTest.java
@@ -2,7 +2,7 @@
  * Copyright(C) Triniforce
  * All Rights Reserved.
  *
- */ 
+ */
 package com.triniforce.server.srvapi;
 
 import java.util.concurrent.SynchronousQueue;
@@ -11,11 +11,10 @@ import com.triniforce.db.test.TFTestCase;
 import com.triniforce.server.plugins.kernel.TaskExecutorsTest;
 
 public class InitFinitTaskWrapperTest extends TFTestCase {
-    
-    
+
     @Override
     public void test() throws Exception {
-        SynchronousQueue q = new SynchronousQueue(); 
+        SynchronousQueue q = new SynchronousQueue();
         InitFinitTask t1 = new TaskExecutorsTest.NamedTestTask("t1", true, q);
         InitFinitTask t2 = new TaskExecutorsTest.NamedTestTask("t2", true, q);
         InitFinitTaskWrapper w1 = new InitFinitTaskWrapper(t1);
@@ -24,6 +23,14 @@ public class InitFinitTaskWrapperTest extends TFTestCase {
         assertEquals(w1, w2);
         assertFalse(w1.equals(w3));
         assertTrue(w2.equals(w1));
+    }
+
+    public void test_isSeriousException() {
+        assertTrue(InitFinitTaskWrapper
+                .isSeriousException(new RuntimeException("")));
+        assertFalse(InitFinitTaskWrapper
+                .isSeriousException(new RuntimeException(
+                        "Unable to complete network request to host")));
     }
 
 }
