9fec516907533b9165e702d0c1bd21b7bfc94f6d
maxim.ge <maxim.ge@gmail.com>
2014-12-01 05:36:27 +0000

    Save flexible reports as xls (Excell) http://www.projectkaiser.com/online?fileid=3058658

---
+++ investigation/com/triniforce/utils/xls/InvXlsTest.java
@@ -0,0 +1,17 @@
+/* 
+ * Copyright(C) Triniforce 
+ * All Rights Reserved. 
+ * 
+ */ 
+
+package com.triniforce.utils.xls;
+
+import com.triniforce.db.test.TFTestCase;
+
+public class InvXlsTest extends TFTestCase{
+	@Override
+	public void test() throws Exception {
+		super.test();
+	}
+
+}
