7d6fd3b358602c6079d7256af60194816f5eab1d
alexeyivakov <AlexeyIvakov@gmail.com>
2015-01-12 09:59:39 +0000

    Sync Queues https://offsxp.office.sigma-soft.ru:8443/sigma/#!379682

---
+++ src/com/triniforce/qsync/impl/DboQSyncActualizer.java
@@ -0,0 +1,33 @@
+/* 
+ * Copyright(C) Triniforce 
+ * All Rights Reserved. 
+ * 
+ */
+package com.triniforce.qsync.impl;
+
+import java.util.List;
+
+import com.triniforce.dbo.DBOActualizer;
+import com.triniforce.dbo.IDBObject;
+import com.triniforce.qsync.intf.IQSyncManager;
+import com.triniforce.server.srvapi.IBasicServer.Mode;
+import com.triniforce.server.srvapi.INamedDbId;
+import com.triniforce.utils.ApiStack;
+
+public class DboQSyncActualizer extends DBOActualizer{
+
+	public DboQSyncActualizer() {
+		super(false, Mode.Running);
+	}
+	
+	@Override
+	public void actualize(List<IDBObject> dboList) {
+		IQSyncManager qsMan = ApiStack.getInterface(IQSyncManager.class);
+		for (IDBObject idbObject : dboList) {
+			DboQsyncQueue qs = (DboQsyncQueue)idbObject;
+			long syncerId = ApiStack.getInterface(INamedDbId.class).createId(qs.getClass().getName());
+			qsMan.registerQueue(qs.getQueueId(), syncerId);
+		}
+	}
+
+}
