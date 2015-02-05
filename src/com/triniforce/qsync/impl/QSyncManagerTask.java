4db2b1276f586c9e2b9bdfc43ba3da4672dc3510
alexeyivakov <AlexeyIvakov@gmail.com>
2015-02-05 09:31:54 +0000

    Sync Queues https://offsxp.office.sigma-soft.ru:8443/sigma/#!379682

--- src/com/triniforce/qsync/impl/QSyncManagerTask.java
+++ src/com/triniforce/qsync/impl/QSyncManagerTask.java
@@ -5,7 +5,9 @@
  */
 package com.triniforce.qsync.impl;
 
+import com.triniforce.qsync.intf.IQSyncManager;
 import com.triniforce.server.plugins.kernel.ep.api.PKEPAPIPeriodicalTask;
+import com.triniforce.utils.ApiStack;
 
 public class QSyncManagerTask extends PKEPAPIPeriodicalTask {
 
@@ -15,8 +17,7 @@ public class QSyncManagerTask extends PKEPAPIPeriodicalTask {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		ApiStack.getInterface(IQSyncManager.class).onEveryMinute();
 	}
 
 }
