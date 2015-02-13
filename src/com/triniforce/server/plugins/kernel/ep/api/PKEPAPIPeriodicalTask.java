bcb37178cc6a1e6945706014d465e11492ebbbe8
maxim.ge <maxim.ge@0957a958-f1c7-11dd-ac65-f74f249cfaac>
2015-02-13 14:28:43 +0000

    Periodical Tasks https://offsxp.office.sigma-soft.ru:8443/sigma/#!384455

--- src/com/triniforce/server/plugins/kernel/ep/api/PKEPAPIPeriodicalTask.java
+++ src/com/triniforce/server/plugins/kernel/ep/api/PKEPAPIPeriodicalTask.java
@@ -6,13 +6,7 @@
 package com.triniforce.server.plugins.kernel.ep.api;
 
 import com.triniforce.server.plugins.kernel.PeriodicalTasksExecutor.BasicPeriodicalTask;
-import com.triniforce.server.srvapi.ISrvSmartTranFactory;
 import com.triniforce.utils.ICommitable;
 
 public abstract class PKEPAPIPeriodicalTask extends BasicPeriodicalTask implements IPKEPAPI, ICommitable{
-    @Deprecated
-    public void commit(){
-        ISrvSmartTranFactory.Helper.commit();
-    }
-
 }
