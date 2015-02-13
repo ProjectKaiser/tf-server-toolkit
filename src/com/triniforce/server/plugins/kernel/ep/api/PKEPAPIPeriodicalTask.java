a206da6ba2733cd42ce1806d807b1699350c0057
maxim.ge <maxim.ge@0957a958-f1c7-11dd-ac65-f74f249cfaac>
2015-02-13 14:50:31 +0000

    comment

--- src/com/triniforce/server/plugins/kernel/ep/api/PKEPAPIPeriodicalTask.java
+++ src/com/triniforce/server/plugins/kernel/ep/api/PKEPAPIPeriodicalTask.java
@@ -9,4 +9,5 @@ import com.triniforce.server.plugins.kernel.PeriodicalTasksExecutor.BasicPeriodi
 import com.triniforce.utils.ICommitable;
 
 public abstract class PKEPAPIPeriodicalTask extends BasicPeriodicalTask implements IPKEPAPI, ICommitable{
+    //
 }
