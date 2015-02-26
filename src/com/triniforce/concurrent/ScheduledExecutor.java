041b238d8f3b558c8e1c16701cc15f21740d5011
maxim.ge <maxim.ge@0957a958-f1c7-11dd-ac65-f74f249cfaac>
2015-02-26 16:14:23 +0000

    ScheduledExecutor https://offsxp.sigma-soft.ru/sigma/#!384948

--- src/com/triniforce/concurrent/ScheduledExecutor.java
+++ src/com/triniforce/concurrent/ScheduledExecutor.java
@@ -5,7 +5,9 @@
  */ 
 package com.triniforce.concurrent;
 
+import java.util.PriorityQueue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.SynchronousQueue;
@@ -15,11 +17,34 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
-public class ScheduledExecutor extends ThreadPoolExecutor implements ScheduledExecutorService{
+public class ScheduledExecutor extends ThreadPoolExecutor implements ScheduledExecutorService, Runnable{
+    
+    abstract class Cmd implements Runnable{
+               
+    }
+    
+    class CmdScheduleTask extends Cmd{
+        private final ScheduledExecutorTask m_task;
+
+        CmdScheduleTask(ScheduledExecutorTask task){
+            m_task = task;
+        }
+
+        public ScheduledExecutorTask getTask() {
+            return m_task;
+        }
+
+        @Override
+        public void run() {
+            m_tasksQueue.add(m_task);
+        }
+    }
+    
+    LinkedBlockingQueue<Cmd> m_CommandQueue = new LinkedBlockingQueue<Cmd>();
+    PriorityQueue<ScheduledExecutorTask> m_tasksQueue = new PriorityQueue<ScheduledExecutorTask>(); 
     
     public static class MyThreadFactory implements ThreadFactory{
 
-        
         AtomicInteger cnt = new AtomicInteger(0); 
         
         @Override
@@ -32,7 +57,8 @@ public class ScheduledExecutor extends ThreadPoolExecutor implements ScheduledEx
     
     public ScheduledExecutor(int corePoolSize, int maxmimumPoolSize) {
         super(corePoolSize + 1, maxmimumPoolSize + 1, 60, TimeUnit.SECONDS,
-        new SynchronousQueue<Runnable>(), new MyThreadFactory());
+                new SynchronousQueue<Runnable>(), new MyThreadFactory());
+        submit(this);
     }
 
     @Override
@@ -54,10 +80,36 @@ public class ScheduledExecutor extends ThreadPoolExecutor implements ScheduledEx
     }
 
     @Override
-    public ScheduledFuture<?> scheduleWithFixedDelay(Runnable paramRunnable,
-            long paramLong1, long paramLong2, TimeUnit paramTimeUnit) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    public ScheduledFuture<?> scheduleWithFixedDelay(Runnable r,
+            long initialDelay, long delay, TimeUnit paramTimeUnit) {
+        long initialDelayMs = paramTimeUnit.convert(initialDelay, TimeUnit.MILLISECONDS);
+        long delayMs = paramTimeUnit.convert(delay, TimeUnit.MILLISECONDS);
+        ScheduledExecutorTask t = new ScheduledExecutorTask(r, initialDelayMs, delayMs);
+        m_CommandQueue.offer(new CmdScheduleTask(t));
+        return t;
     }
-    
+
+    @Override
+    public void run() {
+        while(true){
+            try {
+                ScheduledExecutorTask t = m_tasksQueue.poll();
+                long delayMs;
+                if( null != t){
+                    delayMs = t.getDelay(TimeUnit.MILLISECONDS);
+                }else{
+                    delayMs = 60 * 1000;
+                }
+                Cmd c = m_CommandQueue.poll(delayMs, TimeUnit.MILLISECONDS);
+                if(null != c){
+                    c.run();
+                }
+            } catch (InterruptedException e) {
+                break;
+            }
+            
+        }
+        
+    }    
 
 }
--- test/com/triniforce/concurrent/ScheduledExecutorTaskTest.java
+++ test/com/triniforce/concurrent/ScheduledExecutorTaskTest.java
@@ -6,6 +6,8 @@
 package com.triniforce.concurrent;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import com.triniforce.db.test.TFTestCase;
 import com.triniforce.utils.ApiStack;
@@ -32,6 +34,71 @@ public class ScheduledExecutorTaskTest extends TFTestCase {
         ApiStack.popInterface(1);
         super.tearDown();
     }
+
+    ScheduledExecutorTask newStartedTask(long initialDelayMs, long delayMs){
+        ScheduledExecutorTask res = newTask(initialDelayMs, delayMs);
+        res.calcNextStart();
+        return res;
+    }
+
+    
+    ScheduledExecutorTask newTask(long initialDelayMs, long delayMs){
+        ScheduledExecutorTask res = new ScheduledExecutorTask(null, initialDelayMs, delayMs);
+        res.setTime(new ITime(){
+
+            @Override
+            public long currentTimeMillis() {
+
+                return m_time;
+            }});
+        return res;
+    }
+    
+    public void test_queue() throws InterruptedException{
+        
+        {
+            ScheduledExecutorTask t1 = newStartedTask(1, 2);
+            ScheduledExecutorTask t11 = newStartedTask(11, 2);
+            ScheduledExecutorTask t2 = newStartedTask(2, 2);
+            ScheduledExecutorTask t3 = newStartedTask(3, 2);
+            PriorityBlockingQueue<ScheduledExecutorTask> q = new PriorityBlockingQueue<ScheduledExecutorTask>();
+            
+            q.add(t3);
+            q.add(t2);
+            q.add(t1);
+            q.add(t11);
+            assertSame(t1, q.poll(10, TimeUnit.HOURS));
+            assertSame(t2, q.poll(10, TimeUnit.HOURS));
+            assertSame(t3, q.poll(10, TimeUnit.HOURS));
+            assertSame(t11, q.poll(10, TimeUnit.HOURS));
+        }
+        
+        {
+            ScheduledExecutorTask t1 = newStartedTask(1, 2);
+            ScheduledExecutorTask t2 = newStartedTask(2, 2);
+            ScheduledExecutorTask t3 = newStartedTask(3, 2);
+            PriorityBlockingQueue<ScheduledExecutorTask> q = new PriorityBlockingQueue<ScheduledExecutorTask>();
+            
+            q.add(t2);
+            q.add(t1);
+            assertSame(t1, q.poll());
+            q.add(t3);
+            assertSame(t2, q.poll());
+        }
+        
+        
+        
+    }
+    
+    public void testDelay(){
+        m_time = 0;
+        ScheduledExecutorTask t1 = newTask(1, 2);
+        t1.calcNextStart();
+        assertEquals(1, t1.getDelay(TimeUnit.MILLISECONDS));
+        t1.calcNextStart();
+        assertEquals(2, t1.getDelay(TimeUnit.MILLISECONDS));
+        assertEquals(2000, t1.getDelay(TimeUnit.MICROSECONDS));
+    }
     
     /**
      * Test constructor, calcNextStart and Comparable
@@ -40,9 +107,12 @@ public class ScheduledExecutorTaskTest extends TFTestCase {
     public void test() throws Exception {
         m_time = 0;
         
-        ScheduledExecutorTask t1 = new ScheduledExecutorTask(null, 1, 2);
-        ScheduledExecutorTask t2 = new ScheduledExecutorTask(null, 4, 5);
-        ScheduledExecutorTask t3 = new ScheduledExecutorTask(null, 4, 1);
+        ScheduledExecutorTask t1 = newTask(1, 2);
+        t1.calcNextStart();
+        ScheduledExecutorTask t2 = newTask(4, 5);
+        t2.calcNextStart();
+        ScheduledExecutorTask t3 = newTask(4, 1);
+        t3.calcNextStart();
 
         //1 < 4
         assertTrue(t1.compareTo(t2) < 0);
@@ -63,10 +133,12 @@ public class ScheduledExecutorTaskTest extends TFTestCase {
 
         //run-once task
         {
-            ScheduledExecutorTask runOnce = new ScheduledExecutorTask(null, 7, 0);
+            ScheduledExecutorTask runOnce = newTask(7, 0);
+            assertTrue(runOnce.calcNextStart());
             assertFalse(runOnce.calcNextStart());
             
-            ScheduledExecutorTask runOnce2 = new ScheduledExecutorTask(null, 7, -2);
+            ScheduledExecutorTask runOnce2 = newTask(7, -2);
+            assertTrue(runOnce2.calcNextStart());
             assertFalse(runOnce2.calcNextStart());
         }
 
