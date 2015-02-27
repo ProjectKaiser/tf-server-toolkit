0a096bb5f9b7540637c0a2bf6feef77f9bc639d5
maxim.ge <maxim.ge@0957a958-f1c7-11dd-ac65-f74f249cfaac>
2015-02-27 12:01:11 +0000

    ScheduledExecutor https://offsxp.sigma-soft.ru/sigma/#!384948

--- investigation/com/triniforce/concurrent/ScheduledExecutorRealTest.java
+++ investigation/com/triniforce/concurrent/ScheduledExecutorRealTest.java
@@ -16,41 +16,104 @@ public class ScheduledExecutorRealTest extends TFTestCase {
 	
 	final int N_THREADS = 1000;
 	
-	CountDownLatch m_l = new CountDownLatch(N_THREADS);
-	
 	class MyR implements Runnable{
 		
 		private final int m_n;
 		private int m_cnt = 0;
+        private final boolean m_raise;
+        private final CountDownLatch m_cl;
 		
 
-		public MyR(int n) {
-			m_n = n;
-		}
+        public MyR(CountDownLatch cl, int n, boolean raise) {
+            m_cl = cl;
+            m_n = n;
+            m_raise = raise;
+        }
 
 		@Override
 		public void run() {
 			//trace("Run :" + m_n);
 			m_cnt++;
 			if(m_cnt == 2){
-				m_l.countDown();
+				m_cl.countDown();
 				trace("Finished :" + m_n);
 			}
+			if(m_raise){
+			    ICheckInterrupted.Helper.sleep(1000);
+			    throw new NullPointerException();
+			}
+			
 		}
 	}
 	
 	public void testManyTasks() throws Exception {
+	    
+	    CountDownLatch cl = new CountDownLatch(N_THREADS/2);
 		ScheduledExecutor se = new ScheduledExecutor(2, 4);
 		for(int i = 0; i< N_THREADS; i++){
-			Runnable r = new MyR(i);
+			Runnable r = new MyR(cl, i, false);
 			se.scheduleWithFixedDelay(r, 100, 100, TimeUnit.MILLISECONDS);
 		}
 
-		m_l.await();
+
+		while(!cl.await(1000, TimeUnit.MILLISECONDS)){
+		    trace(cl.getCount());
+		}
 		se.shutdownNow();
 		assertTrue(se.awaitTermination(1000, TimeUnit.MILLISECONDS));
 		
 	}
+	
+    public void testTasksWithExceptions() throws Exception {
+        
+        /*
+         * 
+         * tasks 1 and 2 sleeps, raise exceptions at the end of the sleep
+         * task3 gets RejectedExecutionException (both threads busy)  and must be rescheduled
+         * 
+         * 
+         */
+        final CountDownLatch cl = new CountDownLatch(3);
+        ScheduledExecutor se = new ScheduledExecutor(2, 2);
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+                trace("r1 enters");
+                ICheckInterrupted.Helper.sleep(1000);
+                trace("r1 leaves");
+                cl.countDown();
+                throw new NullPointerException();
+            }
+        };
+        Runnable r2 = new Runnable() {
+            @Override
+            public void run() {
+                trace("r2 enters");
+                ICheckInterrupted.Helper.sleep(1000);
+                trace("r2 leaves");
+                cl.countDown();
+                throw new NullPointerException();
+            }
+        };
+        Runnable r3 = new Runnable() {
+            @Override
+            public void run() {
+                trace("r3 enters");
+                cl.countDown();
+            }
+        };
+
+        se.scheduleWithFixedDelay(r1, 100, 101, TimeUnit.MILLISECONDS);
+        se.scheduleWithFixedDelay(r2, 100, 102, TimeUnit.MILLISECONDS);
+        se.scheduleWithFixedDelay(r3, 200, 103, TimeUnit.MILLISECONDS);
+
+        while(!cl.await(1000, TimeUnit.MILLISECONDS)){
+            trace("cl=" + cl.getCount());
+        }
+        se.shutdownNow();
+        assertTrue(se.awaitTermination(1000, TimeUnit.MILLISECONDS));
+        
+    }	
 	
 	@Override
 	public void test() throws Exception {
