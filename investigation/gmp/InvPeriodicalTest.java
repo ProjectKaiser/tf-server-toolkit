d564c114bf057337e7e1a407603c3e702efd29fa
maxim.ge <maxim.ge@0957a958-f1c7-11dd-ac65-f74f249cfaac>
2015-03-02 14:46:21 +0000

    inv

--- investigation/gmp/InvPeriodicalTest.java
+++ investigation/gmp/InvPeriodicalTest.java
@@ -19,5 +19,46 @@ public class InvPeriodicalTest extends BasicServerTestCase{
         }
 
     }
+    
+    public void testThreads(){
+        Runnable r =new Runnable() {
+            
+            int i;
+            @Override
+            public void run() {
+                while(true){
+                    trace("tick" + i++);
+                    ICheckInterrupted.Helper.sleep(1000);
+                }
+            }
+        };
+        
+        Thread t = new Thread(r);
+        t.start();
+        trace("started");
+        m_server.startPeriodicalTasks();
+        ICheckInterrupted.Helper.sleep(5000);
+        
+    }
+    
+    public static void main(String[] args) {
+        Runnable r =new Runnable() {
+            
+            int i;
+            @Override
+            public void run() {
+                while(true){
+                    System.out.println("tick" + i++);
+                    ICheckInterrupted.Helper.sleep(1000);
+                }
+            }
+        };
+        
+        Thread t = new Thread(r);
+        t.start();
+        System.out.println("started");
+        ICheckInterrupted.Helper.sleep(5000);
+        System.out.println("bye");
+    }
 
 }
