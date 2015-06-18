8a45c998c6e45a54bde7d4e6122b77f816bf8277
maxim.ge <maxim.ge@0957a958-f1c7-11dd-ac65-f74f249cfaac>
2015-06-18 11:28:11 +0000

    expression ContainsId

---
+++ src/com/triniforce/eval/OlBExprContainsId.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright(C) Triniforce
+ * All Rights Reserved.
+ *
+ */ 
+package com.triniforce.eval;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Finds space-seperated id
+ */
+public class OlBExprContainsId  extends OlBExprColumnVsValue{
+	@Override
+    public String getOpName() {
+        return "CONTAINS_ID";
+    }
+
+	@Override
+	Object calcComparableTestValue(Object testValue, Object columnValue) {
+        if(null == testValue){
+            return null;
+        }
+        testValue = testValue.toString().toLowerCase();
+        return Pattern.compile( "(^|\\s)" + Pattern.quote(testValue.toString()) +"($|\\s)", Pattern.CASE_INSENSITIVE);
+	}
+	
+	@Override
+	boolean compareNotNullValues(Object columnValue, Object testValue) {
+        String sv = columnValue.toString().toLowerCase();
+        Matcher m = ((Pattern)testValue).matcher(sv);
+        return m.find();
+	}
+	
+	public OlBExprContainsId() {
+	}
+	
+    public OlBExprContainsId(Object value) {
+        super(value);
+    }
+	
+
+}
