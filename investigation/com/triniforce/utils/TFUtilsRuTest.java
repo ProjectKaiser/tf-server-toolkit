9f64d66f3aae602bc33914effadbef172ce98317
maxim-ge <maxim.ge@gmail.com>
2015-09-16 17:42:35 +0300

    russian names

---
+++ investigation/com/triniforce/utils/TFUtilsRuTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright(C) Triniforce
+ * All Rights Reserved.
+ *
+ */ 
+package com.triniforce.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import com.triniforce.db.test.TFTestCase;
+
+public class TFUtilsRuTest extends TFTestCase {
+	@Override
+	public void test() throws Exception {
+		File ruFolder = new File(getTfTestFolder(), "ru");
+		File out = new File(getTfTestFolder(), "ruout.zip");
+		TFUtils.zipFolder(ruFolder, out);
+	}
+	
+	public void testSimple() throws Exception{
+		File ruFolder = new File(getTfTestFolder(), "ru");
+		File out = new File(getTfTestFolder(), "ruout.zip");
+		
+		
+		String zipFile = out.getAbsolutePath();
+		String sourceDirectory = ruFolder.getAbsolutePath();
+
+		//create byte buffer
+		byte[] buffer = new byte[1024];
+		                         
+		//create object of FileOutputStream
+		FileOutputStream fout = new FileOutputStream(zipFile);
+		                         
+		//create object of ZipOutputStream from FileOutputStream
+		ZipOutputStream zout = new ZipOutputStream(fout);
+
+		//create File object from directory name
+		File dir = new File(sourceDirectory);
+
+		//check to see if this directory exists
+		if(!dir.isDirectory())
+		{
+		       System.out.println(sourceDirectory + " is not a directory");
+		       return;
+		}
+
+		File[] files = dir.listFiles();
+		      
+		for(int i=0; i < files.length ; i++) {
+		 System.out.println("Adding " + files[i].getName());
+		          
+		 //create object of FileInputStream for source file
+		 FileInputStream fin = new FileInputStream(files[i]);
+
+		 zout.putNextEntry(new ZipEntry(files[i].getName()));
+
+		 int length;
+
+		 while((length = fin.read(buffer)) > 0) {
+		  zout.write(buffer, 0, length);
+		 }
+		 zout.closeEntry();
+
+		 //close the InputStream
+		 fin.close();
+		}
+
+		//close the ZipOutputStream
+		zout.close();
+
+		System.out.println("Zip file has been created!");
+	}
+
+}
