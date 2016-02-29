0d9fcd152b3f98836346eb348e5b8cfedeeadf7d
maxim-ge <robinb0>
2016-02-29 19:02:26 +0300

    hmm

---
+++ investigation/gmp/InvJUnit4Test.java
@@ -0,0 +1,26 @@
+package gmp;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class InvJUnit4Test{
+	
+	@Test
+	public void aaa(){
+		System.out.println("aaa");
+	}
+	
+	//
+	
+	@Test
+	public void test3() throws Exception {
+		System.out.println("test3");
+	}
+	
+
+	@BeforeClass
+	public static void before(){
+		System.out.println("before");		
+	}
+	
+}
