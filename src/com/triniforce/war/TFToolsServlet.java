089aafb8a6f25ab79e68d067632a9c32f5402c14
AlexeyWhite <alexeyivakov@gmail.com>
2016-03-17 14:36:51 +0300

    Investigate how to create universal server https://offsxp.office.sigma-soft.ru:8443/sigma/#!422873

---
+++ src/com/triniforce/war/TFToolsServlet.java
@@ -0,0 +1,166 @@
+/* 
+ * Copyright(C) Triniforce 
+ * All Rights Reserved. 
+ * 
+ */
+package com.triniforce.war;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+
+import org.apache.commons.dbcp.BasicDataSource;
+
+import com.triniforce.extensions.IPKExtension;
+import com.triniforce.extensions.IPKExtensionPoint;
+import com.triniforce.server.plugins.kernel.BasicServer;
+import com.triniforce.server.plugins.kernel.TFPooledConnection;
+import com.triniforce.server.srvapi.IPlugin;
+import com.triniforce.server.srvapi.IPooledConnection;
+import com.triniforce.soap.InterfaceDescription;
+import com.triniforce.soap.InterfaceOperationDescription;
+import com.triniforce.soap.MultiClassServiceInvoker;
+import com.triniforce.soap.RequestHandler;
+import com.triniforce.soap.RequestHandler.ReflectServiceInvoker;
+import com.triniforce.soap.SOAPServlet;
+import com.triniforce.soap.SoapInclude;
+import com.triniforce.utils.Api;
+import com.triniforce.utils.ApiAlgs;
+
+public class TFToolsServlet extends SOAPServlet {
+	private static final long serialVersionUID = 1947781333414814776L;
+	static final String INITPARAM_PLUGINS = "plugins";
+	private static final int MAX_ACTIVE_CONNECTIONS = 20;
+	
+	static final String CONTEXT = "java:/comp/env";
+	static final String DATABASE = "tftoolDb";
+	
+	List<Class<? extends IPlugin>> m_endpoints;
+	List<IPlugin> m_plugins;
+	IPooledConnection m_pool;
+	private BasicServer m_server;
+	
+	public TFToolsServlet() {
+		super("", "", null);
+	}
+	
+
+	
+	@Override
+	public void init(ServletConfig arg0) throws ServletException {
+		m_endpoints = new ArrayList<Class<?extends IPlugin>>();
+		m_plugins = new ArrayList<IPlugin>();
+		m_plugins.add(new TFToolsPlugin());
+		String epts = arg0.getInitParameter(INITPARAM_PLUGINS);
+		if(null == epts){
+			ApiAlgs.getLog(this).warn("No plugins extensions");			
+		}
+		else{
+			for(String clsName : epts.split(",")){
+				try {
+					Class<? extends IPlugin> cls = (Class<? extends IPlugin>) Class.forName(clsName);
+					IPlugin plugin = cls.newInstance();
+					m_endpoints.add(cls);
+					m_plugins.add(plugin);
+				} catch (ClassNotFoundException e) {
+					ApiAlgs.getLog(this).error("Classs endpoint not found : " + clsName, e);
+				} catch (InstantiationException e) {
+					ApiAlgs.getLog(this).error("Plugin instanciation error", e);
+				} catch (IllegalAccessException e) {
+					ApiAlgs.getLog(this).error("Plugin instanciation error", e);
+				}
+			}
+		}
+
+		InitialContext initContext;
+		try {
+			initContext = new InitialContext();
+			Context envContext  = (Context)initContext.lookup(CONTEXT);
+			BasicDataSource ds = (BasicDataSource)envContext.lookup(DATABASE);
+			m_pool = new TFPooledConnection(ds, MAX_ACTIVE_CONNECTIONS);
+		} catch (NamingException e) {
+			ApiAlgs.rethrowException(e);
+		}
+		
+		super.init(arg0);
+	}
+
+	@Override
+	public Object createService() {
+		try {
+			m_server = initServer();
+			IPKExtensionPoint svcPoint = m_server.getExtensionPoint(UEPServiceEndoint.class);
+			
+			MultiClassServiceInvoker multiSvc = new MultiClassServiceInvoker();
+			for(Map.Entry<String, IPKExtension> entry : svcPoint.getExtensions().entrySet()){
+				String[] split = entry.getKey().split("\\.");
+				String pfx = split.length > 1 ? split[split.length-2] : "";;
+				Object svc;
+				svc = entry.getValue().getInstance();
+				ReflectServiceInvoker invoker = new RequestHandler.ReflectServiceInvoker(svc);
+				multiSvc.addServiceEndpoint(pfx, invoker);
+			}
+			TFToolsBasicServerInvoker res = new TFToolsBasicServerInvoker(m_server, multiSvc);
+			return res;
+		} catch (Exception e) {
+			ApiAlgs.rethrowException(e);
+			return null;
+		}
+	}
+
+	BasicServer initServer(){
+		try{
+			Api coreApi = new Api();
+			coreApi.setIntfImplementor(IPooledConnection.class, m_pool);
+			
+			BasicServer srv = new BasicServer(coreApi, m_plugins);
+			srv.doRegistration();
+	        if(srv.isDbModificationNeeded()){
+	        	srv.doDbModification();
+	        }
+	        srv.initAndStart();
+			return srv;
+		} catch(Exception e){
+			ApiAlgs.rethrowException(e);
+			return null;
+		}
+	}
+
+
+	@Override
+	protected InterfaceDescription generateInterfaceDescription(
+			InterfaceDescription oldDesc) {
+		
+		try {
+			IPKExtensionPoint svcPoint = m_server.getExtensionPoint(UEPServiceEndoint.class);
+			
+			InterfaceDescription res;
+			ArrayList<InterfaceOperationDescription> ops = new ArrayList<InterfaceOperationDescription>();
+			List<SoapInclude> soapIncls = new ArrayList<SoapInclude>();
+
+			for(Map.Entry<String, IPKExtension> entry : svcPoint.getExtensions().entrySet()){
+				Object svc;
+				svc = entry.getValue().getInstance();
+				Class<? extends Object> cls = svc.getClass();
+				List<InterfaceOperationDescription> endpointOps = m_gen.listInterfaceOperations(
+						cls, true);
+				ops.addAll(endpointOps);
+				SoapInclude anno = cls.getAnnotation(SoapInclude.class);
+				if (null != anno)
+					soapIncls.add(anno);
+			}
+			res = m_gen.parse(oldDesc, ops, getClass().getPackage(), soapIncls);
+			return res;
+		} catch (Exception e) {
+			ApiAlgs.rethrowException(e);
+			return null;
+		}		
+	}
+
+}
---
+++ test/com/triniforce/war/TFToolsServletTest.java
@@ -0,0 +1,147 @@
+/* 
+ * Copyright(C) Triniforce 
+ * All Rights Reserved. 
+ * 
+ */
+package com.triniforce.war;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.sql.Date;
+import java.util.Hashtable;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.spi.InitialContextFactory;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import com.triniforce.db.test.TFTestCase;
+import com.triniforce.extensions.PKPlugin;
+import com.triniforce.soap.InterfaceDescription;
+import com.triniforce.soap.InterfaceDescription.Operation;
+import com.triniforce.utils.ApiStack;
+import com.triniforce.utils.ITime;
+
+public class TFToolsServletTest extends TFTestCase {
+	
+	
+	public static class MyInitialContextFactory implements InitialContextFactory {
+
+	    public Context getInitialContext(Hashtable<?, ?> arg0)
+	            throws NamingException {
+
+	        Context context = Mockito.mock(Context.class);
+	        Mockito.when(context.lookup("java:/comp/env")).thenReturn(context);
+	        Mockito.when(context.lookup("tftoolDb")).thenReturn(getDataSource());
+	        return context;
+	    }
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		System.setProperty(Context.INITIAL_CONTEXT_FACTORY, MyInitialContextFactory.class.getName());
+		
+	}
+	
+	static TestSvc p1 = Mockito.mock(TestSvc.class);
+	
+	public static class TestSvc{
+		public void method_001(){
+			p1.method_001();
+		}
+
+		public long method_002(){
+			return ApiStack.getInterface(ITime.class).currentTimeMillis();
+		}
+		
+	} 
+	
+	public static class TestEP extends PKPlugin{
+
+		@Override
+		public void doRegistration() {
+			putExtension(UEPServiceEndoint.class, TestSvc.class);
+		}
+
+		@Override
+		public void doExtensionPointsRegistration() {
+		}
+	}
+
+	@Override
+	public void test() throws Exception {
+		TFToolsServlet srv = new TFToolsServlet();
+		ServletConfig cfg = Mockito.mock(ServletConfig.class);
+		Mockito.when(cfg.getInitParameter("plugins")).thenReturn(TestEP.class.getName());
+		srv.init(cfg);
+		
+		assertNotNull(srv.getService());
+		
+		InterfaceDescription desc = srv.getInterfaceDescription();
+		List<Operation> ops = desc.getOperations();
+		assertEquals("war_method_001", ops.get(0).getName());
+		
+        assertEquals( "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":null}",
+        		exec(srv, "{\"jsonrpc\":\"2.0\",\"method\":\"war_method_001\",\"params\":[],\"id\":1}"));
+		
+		Mockito.verify(p1).method_001();
+		
+		JSONParser p = new JSONParser();
+		
+		String resp = exec(srv, "{\"jsonrpc\":\"2.0\",\"method\":\"war_method_002\",\"params\":[],\"id\":1}");
+		JSONObject obj = (JSONObject) p.parse(resp);
+		new Date((Long) obj.get("result"));
+	}
+	
+	public class StubServletOutputStream extends ServletOutputStream {
+		public ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+		public void write(int i) throws IOException {
+			baos.write(i);
+		}
+	}
+
+	private String exec(TFToolsServlet srv, String str) throws IOException, ServletException {
+		HttpServletRequest req = Mockito.mock(HttpServletRequest.class);       
+        HttpServletResponse res = Mockito.mock(HttpServletResponse.class);
+        Mockito.when(req.getMethod()).thenReturn("POST");
+        Mockito.when(req.getContentType()).thenReturn("text/json");
+        ServletInputStream in = Mockito.mock(ServletInputStream.class);
+        byte[] myBinaryData = str.getBytes();
+        
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(myBinaryData);
+
+        Mockito.when(in.read(Matchers.<byte[]>any())).thenAnswer(new Answer<Integer>() {
+            @Override
+            public Integer answer(InvocationOnMock invocationOnMock) throws Throwable {
+                Object[] args = invocationOnMock.getArguments();
+                byte[] output = (byte[]) args[0];
+                return byteArrayInputStream.read(output);
+
+            }
+        });
+
+        StubServletOutputStream out = new StubServletOutputStream();
+        Mockito.when(req.getInputStream()).thenReturn(in);
+        Mockito.when(res.getOutputStream()).thenReturn(out);
+		srv.service(req, res);
+		
+		return out.baos.toString();
+	}
+}
