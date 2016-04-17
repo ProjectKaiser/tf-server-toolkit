b7ff152afac8443c72db787930d826ffb65b1f1c
maxim-ge <maxim.ge@gmail.com>
2016-04-17 22:19:51 +0300

    Named params

--- src/com/triniforce/server/soap/LongListRequest.java
+++ src/com/triniforce/server/soap/LongListRequest.java
@@ -6,12 +6,15 @@
 package com.triniforce.server.soap;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.triniforce.soap.PropertiesSequence;
+import com.triniforce.utils.TFUtils;
 
 
-@PropertiesSequence( sequence = {"args", "startFrom", "limit"})
+@PropertiesSequence( sequence = {"args", "startFrom", "limit", "params"})
 public class LongListRequest extends SessionRequest{
     public static final String PROP_START_FROM = "startFrom"; //$NON-NLS-1$
     public static final String PROP_LIMIT = "limit"; //$NON-NLS-1$
@@ -40,6 +43,8 @@ public class LongListRequest extends SessionRequest{
     }
     
     List<Object> m_args = new ArrayList<Object>();
+    
+    private Map<String, Object> m_namedParams = new HashMap<String, Object>();
 
     public List<Object> getArgs() {
         return m_args;
@@ -47,22 +52,15 @@ public class LongListRequest extends SessionRequest{
 
     public static Object MustHaveValueMarker = new Object();
     
-
-    /**
-     * @return args[idx] if it exists and of given class, def otherwise.
-     * <p>Throws IllegalArgumentException(argName) if cls is specified and argument does not match it
-     * <p>If def is {@link LongListRequest#MustHaveValueMarker} and argument does not exist or null then {@link EArgumentMustHaveValue} is thrown. 
-     */
-
-    public static <T> T getArg(List args, int idx, String argName, Class<? extends T> cls, T def){
-        if(idx <0) throw new IllegalArgumentException("idx must be GE zero");
-        if(args.size()<=idx){
-            if( def == MustHaveValueMarker){
-                throw new EArgumentMustHaveValue(argName);
-            }
-            return def;
+    
+    static <T> T processDefault(String argName, Class<? extends T> cls, T def){
+        if( def == MustHaveValueMarker ){
+            throw new EArgumentMustHaveValue(argName);
         }
-        Object res = args.get(idx);
+        return def;
+    }
+    
+    static <T> T checkResult(Object res, String argName, Class<? extends T> cls, T def){
         if( null == res) {
             if( def == MustHaveValueMarker ){
                 throw new EArgumentMustHaveValue(argName);
@@ -74,6 +72,28 @@ public class LongListRequest extends SessionRequest{
         }
         return (T)res;
     }
+
+    /**
+     * @return args[idx] if it exists and of given class, def otherwise.
+     * <p>Throws IllegalArgumentException(argName) if cls is specified and argument does not match it
+     * <p>If def is {@link LongListRequest#MustHaveValueMarker} and argument does not exist or null then {@link EArgumentMustHaveValue} is thrown. 
+     */
+
+    public static <T> T getArg(List args, int idx, String argName, Class<? extends T> cls, T def){
+        if(idx <0) throw new IllegalArgumentException("idx must be GE zero");
+        if(args.size()<=idx){
+        	return processDefault(argName, cls, def);
+        }
+        return checkResult(args.get(idx), argName, cls, def);
+    }
+    
+    public static <T> T getNamedParam(Map<String, Object> params, String paramName, Class<? extends T> cls, T def){
+        if(TFUtils.isEmptyString(paramName)) throw new IllegalArgumentException("paramName must have value");
+        if(!params.containsKey(paramName)){
+        	return processDefault(paramName, cls, def);
+        }
+        return checkResult(params.get(paramName), paramName, cls, def);
+    }
     
     /**
      * @return arg[idx] if it exists and of given class, def otherwise. 
@@ -84,9 +104,18 @@ public class LongListRequest extends SessionRequest{
     	return (T) getArg(m_args, idx, argName, cls, def);
     }
     
+    public <T> T getNamedParam(String paramName, Class<? extends T> cls, Object def){
+    	return (T) getNamedParam(m_namedParams, paramName, cls, def);
+    }
+    
     public void setArgs(List<Object> args) {
         m_args = args;
     }
-    
+	public Map<String, Object> getNamedParams() {
+		return m_namedParams;
+	}
+	public void setNamedParams(Map<String, Object> m_params) {
+		this.m_namedParams = m_params;
+	}
 
 }
--- test/com/triniforce/server/soap/LongListRequestTest.java
+++ test/com/triniforce/server/soap/LongListRequestTest.java
@@ -6,12 +6,45 @@
 package com.triniforce.server.soap;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.triniforce.db.test.TFTestCase;
 
 public class LongListRequestTest extends TFTestCase {
 
+	public void testParams() throws Exception {
+        Map<String, Object> params = new HashMap<String, Object>();
+        try {
+            LongListRequest.getNamedParam(params, "arg1", String.class, LongListRequest.MustHaveValueMarker);
+            fail();
+        } catch (EArgumentMustHaveValue e) {
+            trace(e);
+        }
+        
+        try {
+        	LongListRequest.getNamedParam(params, null, String.class, LongListRequest.MustHaveValueMarker);
+        	fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(e.getMessage().contains("paramName"));
+		}
+        
+        params.put("p1", null);
+        try {
+        	LongListRequest.getNamedParam(params, "p1", String.class, LongListRequest.MustHaveValueMarker);
+            fail();
+        } catch (EArgumentMustHaveValue e) {
+            trace(e);
+        }
+        
+        params.put("p1", 1);
+        assertEquals((Integer)1, LongListRequest.getNamedParam(params, "p1", Integer.class, LongListRequest.MustHaveValueMarker));
+        assertEquals((Integer)2, LongListRequest.getNamedParam(params, "p2", Integer.class, 2));
+        String k = uuid("k");
+        assertEquals(k, LongListRequest.getNamedParam(params, "p3", String.class, k));
+	}
+	
     @Override
     public void test() throws Exception {
 
