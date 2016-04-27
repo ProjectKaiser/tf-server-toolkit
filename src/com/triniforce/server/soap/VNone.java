5ae23e5107d143ed2b6ea80116646521dcc00776
maxim-ge <maxim.ge@gmail.com>
2016-04-27 15:42:20 +0300

    VNone.none

--- src/com/triniforce/server/soap/VNone.java
+++ src/com/triniforce/server/soap/VNone.java
@@ -1,6 +1,13 @@
 package com.triniforce.server.soap;
 
 public class VNone {
+
+	public static final VNone none = new VNone();
+	
+	@Override
+	public boolean equals(Object obj) {
+		return obj instanceof VNone;
+	}
 	
 	@Override
 	public String toString() {
