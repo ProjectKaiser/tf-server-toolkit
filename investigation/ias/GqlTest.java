cf0bbb76add5cabc90e8e3e0e94732fad78b22b1
ias <ias@sigma-soft.spb.ru>
2019-11-21 17:43:34 +0300

    .

---
+++ investigation/ias/GqlTest.java
@@ -0,0 +1,43 @@
+/* 
+ * Copyright(C) Triniforce 
+ * All Rights Reserved. 
+ * 
+ */
+package ias;
+
+import com.triniforce.db.test.TFTestCase;
+
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
+
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
+
+public class GqlTest extends TFTestCase {
+
+	@Override
+	public void test() throws Exception {
+        String schema = "type Query{hello: String}";
+
+        SchemaParser schemaParser = new SchemaParser();
+        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
+
+        RuntimeWiring runtimeWiring = newRuntimeWiring()
+                .type("Query", builder -> builder.dataFetcher("hello", new StaticDataFetcher("world")))
+                .build();
+
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+
+        GraphQL build = GraphQL.newGraphQL(graphQLSchema).build();
+        ExecutionResult executionResult = (ExecutionResult) build.execute("{hello}");
+
+        System.out.println(executionResult.getData().toString());
+	}
+}
