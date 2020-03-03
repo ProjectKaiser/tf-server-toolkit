b4e829e5d7a1d9b19d6c7f2da94bc4d4b48c1bd4
AlekseiIvakov <alexeyivakov@gmail.com>
2020-03-03 18:06:20 +0300

    tst

--- test/com/triniforce/server/plugins/kernel/services/PKEPServicesTest.java
+++ test/com/triniforce/server/plugins/kernel/services/PKEPServicesTest.java
@@ -6,8 +6,6 @@
 package com.triniforce.server.plugins.kernel.services;
 
 import com.triniforce.server.srvapi.IBasicServer;
-import com.triniforce.server.srvapi.IBasicServer.Mode;
-import com.triniforce.server.srvapi.IThrdWatcherRegistrator;
 import com.triniforce.utils.ApiStack;
 
 public class PKEPServicesTest extends ServicesTestCase {
@@ -31,26 +29,16 @@ public class PKEPServicesTest extends ServicesTestCase {
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		getServer().enterMode(Mode.Running);
 		IBasicServer rep = ApiStack.getInterface(IBasicServer.class);
 		ep = (PKEPServices) rep.getExtensionPoint(PKEPServices.class);
 		ep.putExtension(TestService.class);
-		
-        IThrdWatcherRegistrator twr = ApiStack.getInterface(IThrdWatcherRegistrator.class);
-        twr.registerThread(Thread.currentThread(), null);
-
 	}
 	
 	@Override
 	protected void tearDown() throws Exception {
-        IThrdWatcherRegistrator twr = ApiStack.getInterface(IThrdWatcherRegistrator.class);
-        twr.unregisterThread(Thread.currentThread());
-
         IBasicServer rep = ApiStack.getInterface(IBasicServer.class);
 		ep = (PKEPServices) rep.getExtensionPoint(PKEPServices.class);
 		ep.removeExtension(TestService.class.getName());
-        
-		getServer().leaveMode();
 		super.tearDown();
 	}
 
