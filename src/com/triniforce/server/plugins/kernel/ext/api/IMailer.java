5739a17913c84b3eba4dcc60e2824032a7bb5662
kvn <v.kudryavtsev@dev.untill.com>
2022-05-31 15:59:38 +0300

    Extract Attachment from IMailer + settings

--- src/com/triniforce/server/plugins/kernel/ext/api/IMailer.java
+++ src/com/triniforce/server/plugins/kernel/ext/api/IMailer.java
@@ -1,43 +1,10 @@
 package com.triniforce.server.plugins.kernel.ext.api;
 
-import java.io.Serializable;
-
 public interface IMailer {
 	static class EMailerConfigurationError extends Exception{
 		private static final long serialVersionUID = -2931102442738779348L;
 	}
 	
-	public static class Attachment implements Serializable {
-		private static final long serialVersionUID = -5258299556395660257L;
-		private String fileName;
-		private String contentType;
-		private byte[] data;
-		private String description;
-
-		public Attachment(String fileName, String contentType, byte[] data, String description) {
-			this.fileName = fileName;
-			this.contentType = contentType;
-			this.data = data;
-			this.description = description;
-		}
-
-		public String getFileName() {
-			return fileName;
-		}
-
-		public String getContentType() {
-			return contentType;
-		}
-
-		public byte[] getData() {
-			return data;
-		}
-
-		public String getDescription() {
-			return description;
-		}
-	}
-
 	public boolean isMailerConfigured();
 	
 	public boolean send(String from, String to, String subject, String body) throws EMailerConfigurationError;
